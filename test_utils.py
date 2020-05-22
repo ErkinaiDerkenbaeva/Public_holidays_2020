bf8fbc614f3b7b3aac48be6146b2985159eb391b
Daniel Huppmann <dh@dergelbesalon.at>
2020-05-22 11:20:46 +0200

    Remove dependency on pandas, use list

--- test_utils.py
+++ test_utils.py
@@ -1,5 +1,4 @@
-import pandas as pd
 from utils import holiday_announcer
 
 def test_holiday_announcer():
-    assert holiday_announcer('Jun') == pd.Series(['Whit Monday'])
+    assert list(holiday_announcer('Jun')) == ['Whit Monday']
